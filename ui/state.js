3393775d66ee1deba740f4e0d8c66dbb537a19c2
Peter StJ <regardingscot@gmail.com>
2013-06-21 14:09:33 +0300

    Extract the new states for the touch

---
+++ ui/state.js
@@ -0,0 +1,11 @@
+goog.provide('pstj.ui.State');
+
+/**
+ * Some additional states we will need to implement better touchables.
+ * @enum {number}
+ */
+pstj.ui.State = {
+  PRESSED: 0x80,
+  MOVED: 0x100,
+  LONG_PRESSED: 0x200
+};
