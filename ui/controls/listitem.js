854450d206cb2c94ad0892d1f9b255531636e6f5
Peter StJ <regardingscot@gmail.com>
2013-06-21 17:13:31 +0300

    Add exmaple list item control

---
+++ ui/controls/listitem.js
@@ -0,0 +1,33 @@
+goog.provide('pstj.ui.ListItemControl');
+
+goog.require('goog.ui.Control');
+goog.require('pstj.ui.ListItemRenderer');
+
+/**
+ * My new class description
+ * @constructor
+ * @extends {goog.ui.Control}
+ * @param {goog.ui.ControlRenderer} opt_renderer The renderer instance to use.
+ */
+pstj.ui.ListItemControl = function(opt_renderer) {
+  goog.base(this, '', opt_renderer || pstj.ui.ListItemRenderer.getInstance());
+  this.setSupportedState(goog.ui.Component.State.SELECTED, true);
+};
+goog.inherits(pstj.ui.ListItemControl, goog.ui.Control);
+
+goog.scope(function() {
+
+  var _ = pstj.ui.ListItemControl.prototype;
+
+  _.enterDocument = function() {
+    goog.base(this, 'enterDocument');
+    this.getHandler().listen(this, goog.ui.Component.EventType.ACTIVATE,
+      this.onActivate);
+  };
+
+  _.onActivate = function(e) {
+    this.setSelected(true);
+  };
+
+});
+
