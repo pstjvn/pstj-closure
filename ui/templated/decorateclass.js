75c762b6bf500b1aa8f5a0016d13d02b788318c1
PeterStJ <regardingscot@gmail.com>
2013-08-21 09:26:00 -0400

    Remove decorator abstraction as it is hosted in another project

--- ui/templated/decorateclass.js
+++
@@ -1,9 +0,0 @@
-goog.provide('pstj.ui.DecorateClassName');
-
-/**
- * Provides the decorate class name to signify which DOM nodes can be passed
- *   trou automatic decoration.
- * @type {string}
- * @const
- */
-pstj.ui.DecorateClassName = goog.getCssName('decorate');
