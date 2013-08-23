2b1457beed7c36481d7b99c67d6e5924e443f091
PeterStJ <regardingscot@gmail.com>
2013-08-23 08:31:57 -0400

    Update docs and simplify the structure for size monitoring agent

--- ui/agents/agent.js
+++ ui/agents/agent.js
@@ -13,111 +13,117 @@ goog.require('pstj.ds.Cache');
  */
 
 /**
+ * Basic agent implementation. The class is designed to add behaviour to
+ * components without subclassing them and taking into account the lifecycle
+ * abstraction in the google component abstraction. The agent allow a component
+ * to be attached to an agent and the behaviour will be stuck to the component
+ * until it is disposed or explicitly detached.
+ *
  * @constructor
  * @param {*} type The type to check agains the values of the cache.
  */
 pstj.ui.Agent = function(type) {
 
   /**
-   * The list of registered components.
+   * The list of registered components to monitor. At any given time the agent
+   * should be able to access any of the registered and live components from it.
+   *
    * @type {Array.<goog.ui.Component>}
    * @private
    */
   this.components_ = [];
 
   /**
+   * Cache utility for the value to store associcated to any given component
+   * that is attached to the agent.
+   *
    * @private
    * @type {pstj.ds.Cache}
    */
   this.cache_ = new pstj.ds.Cache();
 
   /**
-   * The type to check the values for.
+   * The type to check the values in the cache against.
+   *
    * @type {*}
    * @private
    */
   this.type_ = type || null;
 };
 
-goog.scope(function() {
+/**
+ * Accessor for the cache for subclasses.
+ * @return {pstj.ds.Cache}
+ */
+pstj.ui.Agent.prototype.getCache = function() {
+  return this.cache_;
+};
 
-  var _ = pstj.ui.Agent.prototype;
-
-  /**
-   * Accessor for the cache for subclasses.
-   * @return {pstj.ds.Cache}
-   */
-  _.getCache = function() {
-    return this.cache_;
-  };
-
-  /**
-   * Attaches a component to be notifies on window size changes. If the size
-   *   of the component has changed (i.e. is different from the stored cache)
-   *   the resize event is fired from within the component.
-   *
-   * Note that the component should dispatch the resize event on its own when
-   *   entering the document as only actual window resizes trigger the checks.
-   *   Also it is possible that no cache is added for the component when added
-   *   to the document and when window resize occurs the dispatching of the
-   *   resize event might be triggered even if the actual size of the
-   *   component has not changed. Thus it is advisable to attach the
-   *   components on enterDocument, the component is specifically checked for
-   *   duplicates, thus entering and leasing the document multiple times will
-   *   not affect performance.
-   * @param {goog.ui.Component} component The component to add for monitoring.
-   */
-  _.attach = function(component) {
-    if (!goog.array.contains(this.components_, component)) {
-      var nullindex = goog.array.indexOf(this.components_, null);
-      if (nullindex != -1) {
-        this.components_[nullindex] = component;
-      } else {
-        this.components_.push(component);
-      }
-      component.addOnDisposeCallback(goog.bind(this.detach, this, component));
-      this.updateCache(component);
+/**
+ * Attaches a component to the agent. The cache will be set at this point. The
+ * method explicitly checks if the component has already been registered with
+ * the agent instance and if yes the step is skipped. This allows a component
+ * to be attached more than once safely. The attach method also looks up any
+ * empty spots in the component registry list and tries to fill them up in
+ * order to consume less memory and create less garbage.
+ *
+ * @param {goog.ui.Component} component The component to add for monitoring.
+ */
+pstj.ui.Agent.prototype.attach = function(component) {
+  if (!goog.array.contains(this.components_, component)) {
+    var nullindex = goog.array.indexOf(this.components_, null);
+    if (nullindex != -1) {
+      this.components_[nullindex] = component;
+    } else {
+      this.components_.push(component);
     }
-  };
+    component.addOnDisposeCallback(goog.bind(this.detach, this, component));
+    this.updateCache(component);
+  }
+};
 
-  /**
-   * Checks internally the type of a cache value.
-   * @param {*} val The value to check for type.
-   */
-  _.checkValue = function(val) {
-    if (goog.isDefAndNotNull(this.type_) &&
-      goog.asserts.assertFunction(this.type_)) {
-      goog.asserts.assertInstanceof(val, this.type_);
-    }
-  };
+/**
+ * Checks internally the type of a cache value. The checks is performed only for
+ * construct types and only if the type is set. If DEBUG is disbaled this
+ * method is not used.
+ *
+ * @param {*} val The value to check for type.
+ */
+pstj.ui.Agent.prototype.checkValue = function(val) {
+  if (goog.isDefAndNotNull(this.type_) &&
+    goog.asserts.assertFunction(this.type_)) {
+    goog.asserts.assertInstanceof(val, this.type_);
+  }
+};
 
-  /**
-   * Protected method that should update whatever type of cache we have on the
-   *   component.
-   * @param {goog.ui.Component} component The component that needs cache
-   *   update.
-   * @protected
-   */
-  _.updateCache = function(component) {};
+/**
+ * Protected method that should update whatever type of cache we have on the
+ * component.
+ *
+ * @param {goog.ui.Component} component The component that needs cache update.
+ * @protected
+ */
+pstj.ui.Agent.prototype.updateCache = function(component) {};
 
-  /**
-   * Call function for each cached component.
-   * @param {function(this: T, S, number, Array.<S>): undefined} fn The function to execute.
-   * @param {T=} obj The this object.
-   * @template T,S
-   */
-  _.forEach = function(fn, obj) {
-    goog.array.forEach(this.components_, fn, obj);
-  };
+/**
+ * Call function for each cached component.
+ *
+ * @param {function(this: T, S, number, Array.<S>): undefined} fn The function to execute.
+ * @param {T=} obj The this object.
+ * @template T,S
+ */
+pstj.ui.Agent.prototype.forEach = function(fn, obj) {
+  goog.array.forEach(this.components_, fn, obj);
+};
 
-  /**
-   * Removes the binding of the component to this agent.
-   * @param {goog.ui.Component} component The component to remove.
-   */
-  _.detach = function(component) {
-    if (goog.array.contains(this.components_, component)) {
-      this.cache_.remove(component.getId());
-      this.components_[goog.array.indexOf(this.components_, component)] = null;
-    }
-  };
-});
+/**
+ * Removes the binding of the component to this agent.
+ *
+ * @param {goog.ui.Component} component The component to remove.
+ */
+pstj.ui.Agent.prototype.detach = function(component) {
+  if (goog.array.contains(this.components_, component)) {
+    this.cache_.remove(component.getId());
+    this.components_[goog.array.indexOf(this.components_, component)] = null;
+  }
+};
--- ui/agents/sizemonitor-agent.js
+++ ui/agents/sizemonitor-agent.js
@@ -12,106 +12,100 @@ goog.require('pstj.ui.Agent');
 
 /**
  * @fileoverview Size monitor agent is used to allow components to receive
- *   notifications about the window resize activities without actually
- *   inheriting this capability from ancestor. This should simplify the
- *   inheritance tree.
+ * notifications about the window resize activities without actually
+ * inheriting this capability from ancestor and the behaviour could be
+ * applied to any component.
  *
  * @author regardingscot@gmail.com (Peter StJ)
  */
 
 /**
  * Allows component instances to receive size change events from window size
- *   monitor.
+ * monitor without actually implementing the monitoring and related code paths
+ * for throthling etc.
+ *
+ * Note that the component should dispatch the resize event on its own when
+ * entering the document as only actual window resizes trigger the checks.
+ * Also it is possible that no cache is added for the component when added
+ * to the document and when window resize occurs the dispatching of the
+ * resize event might be triggered even if the actual size of the
+ * component has not changed. Thus it is advisable to attach the
+ * components on enterDocument, the component is specifically checked for
+ * duplicates, thus entering and leaving the document multiple times will
+ * not affect performance.
+ *
  * @constructor
  * @extends {pstj.ui.Agent}
  */
 pstj.ui.SizeMonitorAgent = function() {
   goog.base(this, goog.math.Size);
-
-  /**
-   * @private
-   * @type {goog.async.Throttle}
-   */
-  this.handleWindowResizeThrottle_ = new goog.async.Throttle(
-    this.handleWindowResize_, this.handleResizeInterval, this);
-
-  /**
-   * Reference to the view port size monitor.
-   * @type {goog.dom.ViewportSizeMonitor}
-   * @private
-   */
-  this.sizeMonitorInstance_ =
-    goog.dom.ViewportSizeMonitor.getInstanceForWindow();
-
   // bind listener for resize monitoring.
-  goog.events.listen(this.sizeMonitorInstance_, goog.events.EventType.RESIZE,
-    this.handleWindowResizeThrottle_);
+  goog.events.listen(goog.dom.ViewportSizeMonitor.getInstanceForWindow(),
+    goog.events.EventType.RESIZE, (new goog.async.Throttle(
+      this.handleWindowResize_, pstj.ui.SizeMonitorAgent.THROTHLE_INTERVAL,
+      this)));
 };
 goog.inherits(pstj.ui.SizeMonitorAgent, pstj.ui.Agent);
 goog.addSingletonGetter(pstj.ui.SizeMonitorAgent);
 
-goog.scope(function() {
+/**
+ * @define {number} Number of milliseconds to wait before allowsing invokation
+ * of the real event handler for the resize event in size monigor agent for UI
+ * components.
+ */
+goog.define('pstj.ui.SizeMonitorAgent.THROTHLE_INTERVAL', 500);
 
-  var _ = pstj.ui.SizeMonitorAgent.prototype;
+/**
+ * Handles the resize event from the viewport monitor after throttling.
+ *
+ * @private
+ */
+pstj.ui.SizeMonitorAgent.prototype.handleWindowResize_ = function() {
+  this.forEach(this.notifyResize_, this);
+};
 
-  /**
-   * Throttle for the size checks to perform on resize window events. The
-   *   value is considered to be in milliseconds.
-   * @type {number}
-   * @protected
-   */
-  _.handleResizeInterval = 500;
+/**
+ * Iterative callback for each component currently attached to listen for
+ * resize of the window.
+ *
+ * @param {goog.ui.Component} item The component / item to check.
+ * @private
+ */
+pstj.ui.SizeMonitorAgent.prototype.notifyResize_ = function(item) {
+  if (!goog.isNull(item) && item.isInDocument()) {
+    var newSize = goog.style.getSize(item);
+    if (!goog.math.Size.equals(
+      this.checkValue(this.getCache().get(item.getId())) || null, newSize)) {
 
-  /**
-   * Handles the resize event from the viewport monitor after throttling.
-   * @private
-   */
-  _.handleWindowResize_ = function() {
-    this.forEach(this.notifyResize_, this);
-  };
+      this.getCache().set(item.getId(), newSize);
+      item.dispatchEvent(goog.events.EventType.RESIZE);
 
-  /**
-   * Iterative callback for each component currently attached to listen for
-   *   resize of the window.
-   * @param {goog.ui.Component} item The component / item to check.
-   * @private
-   */
-  _.notifyResize_ = function(item) {
-    if (!goog.isNull(item) && item.isInDocument()) {
-      var newSize = goog.style.getSize(item);
-      if (!goog.math.Size.equals(
-        this.checkValue(this.getCache().get(item.getId())) || null, newSize)) {
-
-        this.getCache().set(item.getId(), newSize);
-        // TODO: async this one with next tick
-        item.dispatchEvent(goog.events.EventType.RESIZE);
-      }
     }
-  };
+  }
+};
 
-  /** @inheritDoc */
-  _.updateCache = function(comp) {
-    if (comp.isInDocument()) {
-      this.getCache().set(comp.getId(), goog.style.getSize(comp.getElement()));
-    }
-  };
+/** @inheritDoc */
+pstj.ui.SizeMonitorAgent.prototype.updateCache = function(comp) {
+  if (comp.isInDocument()) {
+    this.getCache().set(comp.getId(), goog.style.getSize(comp.getElement()));
+  }
+};
 
-  /**
-   * Returns the cached size of a component. Notice that if the component is
-   *   not already attached in size monitor it will be. If the component is
-   *   not in the document we cannot determine the size and a cached version
-   *   will be returned. If no cached version is available (for example the
-   *   component was never in the document and was just attached but is not in
-   *   the document) null will be returned because we cannot determine the
-   *   size really.
-   * @param {goog.ui.Component} component The component to check the cached
-   *   size of.
-   * @return {goog.math.Size} The cached size or null if we cannot retrieve
-   *   it.
-   */
-  _.getCachedSize = function(component) {
-    this.attach(component);
-    return this.checkValue(this.getCache().get(component.getId())) || null;
-  };
-
-});
+/**
+ * Returns the cached size of a component. Notice that if the component is
+ * not already attached in size monitor it will be. If the component is
+ * not in the document we cannot determine the size and a cached version
+ * will be returned. If no cached version is available (for example the
+ * component was never in the document and was just attached but is not in
+ * the document) null will be returned because we cannot determine the
+ * size.
+ *
+ * @param {goog.ui.Component} component The component to check the cached
+ * size of.
+ * @return {goog.math.Size} The cached size or null if we cannot retrieve
+ * it.
+ */
+pstj.ui.SizeMonitorAgent.prototype.getCachedSize = function(component) {
+  this.attach(component);
+  return this.checkValue(this.getCache().get(component.getId())) || null;
+};
