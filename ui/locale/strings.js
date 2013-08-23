74fe829998f698bb9b1ae98f875ad47cb68d8015
PeterStJ <regardingscot@gmail.com>
2013-08-23 10:25:34 -0400

    Fix typoe in docs

--- ui/locale/strings.js
+++ ui/locale/strings.js
@@ -1,7 +1,19 @@
 goog.provide('pstj.ui.Strings');
 
 /**
- * Generates the static strings for an application.
+ * @fileoverview Utility class to handle translatable static strings from
+ * templates. It is assumed that the strings are static (i.e no variables in
+ * them) and that those are separated by the '|' sign. The code should address
+ * them by the index of the array of strings resulting from splitting the
+ * message from the template using the '|' separator.
+ *
+ * @author regardingscot@gmail.com (Peter StJ)
+ */
+
+/**
+ * Generates list of static strings from a given translatable string, usually
+ * received by invoking a template.
+ *
  * @param {string} str
  * @constructor
  */
@@ -14,8 +26,11 @@ pstj.ui.Strings = function(str) {
 };
 
 /**
- * Getter for the index string. If such index does not exists an empty string is returned.
- * @param {number} stringIndex The index of the string, usually coming from an enum in your app.
+ * Getter for the index string. If such index does not exists an empty string
+ * is returned.
+ *
+ * @param {number} stringIndex The index of the string, usually coming from an
+ * enum in your app.
  * @return {string}
  */
 pstj.ui.Strings.prototype.get = function(stringIndex)  {
