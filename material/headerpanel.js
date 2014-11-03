ecf56f28f7820f7ed8ac3645b50df1db1fbaa1ed
Peter StJ <regardingscot@gmail.com>
2014-11-03 17:51:36 +0200

    Rework the header panel to make it possible to create instances from JSON.

--- material/headerpanel.js
+++ material/headerpanel.js
@@ -6,175 +6,227 @@
  */
 
 goog.provide('pstj.material.HeaderPanel');
+goog.provide('pstj.material.HeaderPanelRenderer');
 
 goog.require('goog.ui.Component.State');
+goog.require('pstj.agent.Scroll');
+goog.require('pstj.agent.ScrollEvent');
 goog.require('pstj.material.Element');
+goog.require('pstj.material.ElementRenderer');
 goog.require('pstj.material.EventMap');
-goog.require('pstj.material.HeaderPanelRenderer');
 goog.require('pstj.material.State');
+goog.require('pstj.material.template');
 
-
-goog.scope(function() {
-var Base = pstj.material.Element;
-var State = goog.ui.Component.State;
-var EventMap = pstj.material.EventMap;
 
 
 /**
- * Implementation for the header panel material element
+ * The header panel implementation for material element.
+ * @constructor
+ * @struct
+ * @extends {pstj.material.Element}
+ * @param {goog.ui.ControlContent=} opt_content Text caption or DOM structure
+ *     to display as the content of the control (if any).
+ * @param {goog.ui.ControlRenderer=} opt_renderer Renderer used to render or
+ *     decorate the component; defaults to {@link goog.ui.ControlRenderer}.
+ * @param {goog.dom.DomHelper=} opt_domHelper Optional DOM helper, used for
+ *     document interaction.
  */
-pstj.material.HeaderPanel = goog.defineClass(Base, {
-  /**
-   * The header panel implementation for material element.
-   * @constructor
-   * @struct
-   * @extends {Base}
-   * @param {goog.ui.ControlContent=} opt_content Text caption or DOM structure
-   *     to display as the content of the control (if any).
-   * @param {goog.ui.ControlRenderer=} opt_renderer Renderer used to render or
-   *     decorate the component; defaults to {@link goog.ui.ControlRenderer}.
-   * @param {goog.dom.DomHelper=} opt_domHelper Optional DOM helper, used for
-   *     document interaction.
-   */
-  constructor: function(opt_content, opt_renderer, opt_domHelper) {
-    Base.call(this, opt_content, opt_renderer, opt_domHelper);
-    // Enable tall and shadow mode so we can set them is needed
-    this.setSupportedState(State.TALL, true);
-    this.setSupportedState(State.SHADOW, true);
-    this.setSupportedState(State.TRANSITIONING, true);
-    // Enable SCROLL as auto event so we can always listen for scrolls.
-    this.setAutoEventsInternal(EventMap.EventFlag.SCROLL);
-  },
+pstj.material.HeaderPanel = function(opt_content, opt_renderer, opt_domHelper) {
+  goog.base(this, opt_content, opt_renderer, opt_domHelper);
+  // Enable tall and shadow mode so we can set them is needed
+  this.setSupportedState(goog.ui.Component.State.STANDARD, true);
+  this.setSupportedState(goog.ui.Component.State.TALL, true);
+  this.setSupportedState(goog.ui.Component.State.WATERFALL_TALL, true);
+  this.setSupportedState(goog.ui.Component.State.WATERFALL, true);
+  this.setSupportedState(goog.ui.Component.State.SHADOW, true);
+  this.setSupportedState(goog.ui.Component.State.TRANSITIONING, true);
+
+  this.setAutoEventsInternal(pstj.material.EventMap.EventFlag.SCROLL);
+};
+goog.inherits(pstj.material.HeaderPanel, pstj.material.Element);
 
 
-  /**
-   * Easier header element retrieval.
-   * @return {Base}
-   */
-  getHeader: function() {
-    return goog.asserts.assertInstanceof(this.getChildAt(0), Base);
-  },
-
-
-  /**
-   * Getter for the main element of the header panel.
-   * @return {Base}
-   */
-  getMain: function() {
-    return goog.asserts.assertInstanceof(this.getChildAt(1), Base);
-  },
-
-
-  /**
-   * Adds a new component to the header of the header panel.
-   * @param {goog.ui.Component} child
-   * @param {boolean=} opt_render
-   */
-  addToHeader: function(child, opt_render) {
-    this.getHeader().addChild(child, opt_render);
-  },
-
-
-  /**
-   * Adds a new component to the main section of the header panel.
-   * @param {goog.ui.Component} child
-   * @param {boolean=} opt_render
-   */
-  addToMain: function(child, opt_render) {
-    this.getMain().addChild(child, opt_render);
-  },
-
-
-  /** @override */
-  getScrollElement: function() {
-    if (this.isOuterScroll()) {
-      return this.querySelector('.' + goog.getCssName(
-          this.getRenderer().getCssClass(), 'outer-container'));
-    } else {
-      return this.querySelector('.' + goog.getCssName(
-          this.getRenderer().getCssClass(), 'main-container'));
-    }
-  },
-
-
-  /** @override */
-  onScroll: function() {
-    var atTop = this.getScrollElement().scrollTop == 0;
-    this.setState(State.SHADOW,
-        (this.hasState(State.STANDARD) ||
-        (this.isShadow() && !atTop)));
-    this.setState(State.TALL, (this.isTall() && atTop));
-  },
-
-
-  setState: function(state, enable) {
-    if (state == State.TALL) {
-      var oldTall = this.hasState(State.TALL);
-    }
-    goog.base(this, 'setState', state, enable);
-    if (state == State.TALL) {
-      if (oldTall && !this.isTall()) {
-        this.getHandler().listenOnce(this.getHeader().getElement(),
-            goog.events.EventType.TRANSITIONEND, function() {
-              this.setState(State.TRANSITIONING, false);
-            });
-      } else {
-        this.setState(State.TRANSITIONING, true);
-      }
-    }
-  },
-
-  /** @override */
-  enterDocument: function() {
-    goog.base(this, 'enterDocument');
-    this.onScroll();
-  },
-
-
-  isShadow: function() {
-    return !!(this.getState() & pstj.material.HeaderPanel.SHADOW_MODE);
-  },
-
-
-  isTall: function() {
-    return !!(this.getState() & pstj.material.HeaderPanel.TALL_MODE);
-  },
-
-
-  isOuterScroll: function() {
-    return !!(this.getState() & pstj.material.HeaderPanel.OUTERSCROLL_MODE);
-  },
-
-
-  statics: {
-
-
-    /**
-     * Combination of modes in each of them we need the shadow.
-     * @type {number}
-     * @final
-     */
-    SHADOW_MODE: (State.WATERFALL | State.WATERFALL_TALL | State.STANDARD),
-
-
-    /**
-     * Combination of modes in wach of those we need to have tall mode.
-     * @type {number}
-     * @final
-     */
-    TALL_MODE: (State.WATERFALL_TALL),
-
-
-    /**
-     * Combination of modes in each of whose we need to use the outer scroll
-     * container.
-     * @type {number}
-     * @final
-     */
-    OUTERSCROLL_MODE: (State.SCROLL)
+/**
+ * Create a new instance of the header panel from a config file.
+ * @param {MaterialConfig} json
+ * @return {pstj.material.HeaderPanel}
+ */
+pstj.material.HeaderPanel.fromJSON = function(json) {
+  var i = new pstj.material.HeaderPanel();
+  i.setUseScrollAgent(json.useScroll || false);
+  pstj.material.Element.fromJSON(json, i);
+  if (json.type) {
+    i.setType(json.type);
   }
+  return i;
+};
 
-});
+
+/**
+ * Getter for the main element of the header panel.
+ * @return {pstj.material.Element}
+ */
+pstj.material.HeaderPanel.prototype.getMain = function() {
+  return goog.asserts.assertInstanceof(this.getChildAt(1),
+      pstj.material.Element);
+};
+
+
+/**
+ * Easier header element retrieval.
+ * @return {pstj.material.Element}
+ */
+pstj.material.HeaderPanel.prototype.getHeader = function() {
+  return goog.asserts.assertInstanceof(this.getChildAt(0),
+      pstj.material.Element);
+};
+
+
+/** @override */
+pstj.material.HeaderPanel.prototype.getScrollElement = function() {
+  return goog.asserts.assert(this.getContentElement());
+};
+
+
+/** @override */
+pstj.material.HeaderPanel.prototype.onScroll = function(e) {
+  var atTop = e.scrollTop == 0;
+  this.setState(goog.ui.Component.State.SHADOW,
+      (this.hasState(goog.ui.Component.State.STANDARD) ||
+      (this.isShadow() && !atTop)));
+  this.setState(goog.ui.Component.State.TALL, (this.isTall() && atTop));
+};
+
+
+/** @override */
+pstj.material.HeaderPanel.prototype.setState = function(state, enable) {
+  if (state == goog.ui.Component.State.TALL) {
+    var oldTall = this.hasState(goog.ui.Component.State.TALL);
+  }
+  goog.base(this, 'setState', state, enable);
+  if (state == goog.ui.Component.State.TALL) {
+    if (oldTall && !this.isTall()) {
+      this.getHandler().listenOnce(this.getHeader().getElement(),
+          goog.events.EventType.TRANSITIONEND, function() {
+            this.setState(goog.ui.Component.State.TRANSITIONING, false);
+          });
+    } else {
+      this.setState(goog.ui.Component.State.TRANSITIONING, true);
+    }
+  }
+};
+
+
+/** @override */
+pstj.material.HeaderPanel.prototype.enterDocument = function() {
+  goog.base(this, 'enterDocument');
+  if (this.hasUseScrollAgent()) {
+    // simulate a scroll event for the initial tweaking of the UI
+    this.dispatchEvent(new pstj.agent.ScrollEvent(this));
+  }
+};
+
+
+/**
+ * Checks if we have the shadow state set.
+ * @return {boolean}
+ */
+pstj.material.HeaderPanel.prototype.isShadow = function() {
+  return !!(this.getState() & pstj.material.HeaderPanel.SHADOW_MODE);
+};
+
+
+/**
+ * Checks if we have the tell state set on.
+ * @return {boolean}
+ */
+pstj.material.HeaderPanel.prototype.isTall = function() {
+  return !!(this.getState() & pstj.material.HeaderPanel.TALL_MODE);
+};
+
+
+/**
+ * Sets the type of the header panel. The types should be exported.
+ * @param {string} type The type of header panel to use.
+ */
+pstj.material.HeaderPanel.prototype.setType = function(type) {
+  this.setState(goog.ui.Component.State.STANDARD, false);
+  this.setState(goog.ui.Component.State.WATERFALL, false);
+  this.setState(goog.ui.Component.State.WATERFALL_TALL, false);
+  switch (type) {
+    case 'standard':
+      this.setState(goog.ui.Component.State.STANDARD, true);
+      break;
+    case 'waterfall':
+      this.setState(goog.ui.Component.State.WATERFALL, true);
+      break;
+    case 'tall':
+      this.setState(goog.ui.Component.State.WATERFALL_TALL, true);
+      break;
+  }
+};
+
+
+/**
+ * Combination of modes in each of them we need the shadow.
+ * @type {number}
+ * @final
+ */
+pstj.material.HeaderPanel.SHADOW_MODE = (
+    goog.ui.Component.State.WATERFALL |
+    goog.ui.Component.State.WATERFALL_TALL |
+    goog.ui.Component.State.STANDARD);
+
+
+/**
+ * Combination of modes in wach of those we need to have tall mode.
+ * @type {number}
+ * @final
+ */
+pstj.material.HeaderPanel.TALL_MODE = (goog.ui.Component.State.WATERFALL_TALL);
+
+
+
+/**
+ * Do not use the constructor, instead use the getInstance satic method.
+ * @constructor
+ * @extends {pstj.material.ElementRenderer}
+ * @struct
+ */
+pstj.material.HeaderPanelRenderer = function() {
+  goog.base(this);
+};
+goog.inherits(pstj.material.HeaderPanelRenderer, pstj.material.ElementRenderer);
+goog.addSingletonGetter(pstj.material.HeaderPanelRenderer);
+
+
+/**
+ * The CSS name to recognize the control.
+ * @type {string}
+ * @final
+ */
+pstj.material.HeaderPanelRenderer.CSS_CLASS = goog.getCssName(
+    'material-header-panel');
+
+
+/** @override */
+pstj.material.HeaderPanelRenderer.prototype.getTemplate = function(model) {
+  return pstj.material.template.HeaderPanel(model);
+};
+
+
+/** @inheritDoc */
+pstj.material.HeaderPanelRenderer.prototype.getContentElement = function(el) {
+  return goog.dom.getElementByClass(goog.getCssName(this.getCssClass(),
+      'outer-container'), el);
+};
+
+
+/** @override */
+pstj.material.HeaderPanelRenderer.prototype.getCssClass = function() {
+  return pstj.material.HeaderPanelRenderer.CSS_CLASS;
+};
+
 
 goog.ui.registry.setDefaultRenderer(pstj.material.HeaderPanel,
     pstj.material.HeaderPanelRenderer);
@@ -184,5 +236,3 @@ goog.ui.registry.setDecoratorByClassName(
     pstj.material.HeaderPanelRenderer.CSS_CLASS, function() {
       return new pstj.material.HeaderPanel(null);
     });
-});  // goog.scope
-
---
+++ material/headerpanelheader.js
@@ -0,0 +1,88 @@
+goog.provide('pstj.material.HeaderPanelHeader');
+goog.provide('pstj.material.HeaderPanelHeaderRenderer');
+
+goog.require('goog.ui.registry');
+goog.require('pstj.material.Element');
+goog.require('pstj.material.ElementRenderer');
+goog.require('pstj.material.template');
+
+
+
+/**
+ * The implementation for the Custom element.
+ * @constructor
+ * @struct
+ * @extends {pstj.material.Element}
+ * @param {goog.ui.ControlContent=} opt_content Text caption or DOM structure
+ *     to display as the content of the control (if any).
+ * @param {goog.ui.ControlRenderer=} opt_renderer Renderer used to render or
+ *     decorate the component; defaults to {@link goog.ui.ControlRenderer}.
+ * @param {goog.dom.DomHelper=} opt_domHelper Optional DOM helper, used for
+ *     document interaction.
+ */
+pstj.material.HeaderPanelHeader = function(
+    opt_content, opt_renderer, opt_domHelper) {
+  goog.base(this, opt_content, opt_renderer, opt_domHelper);
+};
+goog.inherits(pstj.material.HeaderPanelHeader, pstj.material.Element);
+
+
+/**
+ * Creates a new instance from a JSON config.
+ * @param {MaterialConfig} json
+ * @return {pstj.material.HeaderPanelHeader}
+ */
+pstj.material.HeaderPanelHeader.fromJSON = function(json) {
+  var i = new pstj.material.HeaderPanelHeader();
+  return i;
+};
+
+
+
+/**
+ * Implements the renderer for the element.
+ * @constructor
+ * @struct
+ * @extends {pstj.material.ElementRenderer}
+ */
+pstj.material.HeaderPanelHeaderRenderer = function() {
+  goog.base(this);
+};
+goog.inherits(pstj.material.HeaderPanelHeaderRenderer,
+    pstj.material.ElementRenderer);
+goog.addSingletonGetter(pstj.material.HeaderPanelHeaderRenderer);
+
+
+/**
+ * @type {string}
+ * @final
+ */
+pstj.material.HeaderPanelHeaderRenderer.CSS_CLASS = goog.getCssName(
+    'material-header-panel-header');
+
+
+/** @inheritDoc */
+pstj.material.HeaderPanelHeaderRenderer.prototype.getCssClass = function() {
+  return pstj.material.HeaderPanelHeaderRenderer.CSS_CLASS;
+};
+
+
+/** @inheritDoc */
+pstj.material.HeaderPanelHeaderRenderer.prototype.getTemplate = function(
+    model) {
+  return pstj.material.template.HeaderPanelHeader(model);
+};
+
+
+
+// Register for default renderer.
+goog.ui.registry.setDefaultRenderer(pstj.material.HeaderPanelHeader,
+    pstj.material.HeaderPanelHeaderRenderer);
+
+
+// Register decorator factory function.
+goog.ui.registry.setDecoratorByClassName(
+    pstj.material.HeaderPanelHeaderRenderer.CSS_CLASS, function() {
+      return new pstj.material.HeaderPanelHeader(null);
+    });
+
