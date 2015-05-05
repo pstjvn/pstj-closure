c385592c6d63feb63a0f04e5950eaeb875b9c0f7
Peter StJ <regardingscot@gmail.com>
2015-05-05 13:14:19 +0300

    Simplified menu item

--- material/menuitem.js
+++ material/menuitem.js
@@ -3,28 +3,19 @@ goog.provide('pstj.material.MenuItemRenderer');
 
 goog.require('goog.ui.Component.State');
 goog.require('goog.ui.registry');
-goog.require('pstj.ds.ListItem');
 goog.require('pstj.material.Element');
 goog.require('pstj.material.ElementRenderer');
 goog.require('pstj.material.IconContainer');
 goog.require('pstj.material.Item');
-goog.require('pstj.material.State');
 goog.require('pstj.material.template');
 
 goog.scope(function() {
 var ER = pstj.material.ElementRenderer;
 
 
-/**
- * The implementation for the material menu item.
- * By default it is a simple container with padding (like the material Item).
- * Additionally it can have an icon.
- */
+/** @extends {pstj.material.Element} */
 pstj.material.MenuItem = goog.defineClass(pstj.material.Element, {
   /**
-   * @constructor
-   * @struct
-   * @extends {pstj.material.Element}
    * @param {goog.ui.ControlContent=} opt_content Text caption or DOM structure
    *     to display as the content of the control (if any).
    * @param {goog.ui.ControlRenderer=} opt_renderer Renderer used to render or
@@ -35,64 +26,20 @@ pstj.material.MenuItem = goog.defineClass(pstj.material.Element, {
   constructor: function(opt_content, opt_renderer, opt_domHelper) {
     goog.base(this, opt_content, opt_renderer, opt_domHelper);
     this.setSupportedState(goog.ui.Component.State.EMPTY, true);
+    this.setSupportedState(goog.ui.Component.State.DISABLED, true);
     this.setSupportedState(goog.ui.Component.State.SELECTED, true);
     // by default we do not have an icon.
     this.setEmpty(true);
   },
 
-
-  enterDocument: function() {
-    goog.base(this, 'enterDocument');
-    this.setIcon();
-  },
-
-
-  setModel: function(model) {
-    this.swapModelItem(model);
-    goog.base(this, 'setModel', model);
-    if (this.isInDocument()) {
-    }
-  },
-
-
   /**
-   * Updates the content from the current model.
+   * Updates the icon in the menu item.
+   * @param {pstj.material.icon.Name} icon The icon name.
    */
-  setContentFromModel: function() {
-    var content = '';
-    if (!goog.isNull(this.getModel())) {
-      content = this.getModel().getProp('content');
-      if (goog.isNull(content)) {
-        content = '';
-      }
-    }
-    this.setContent(content);
-  },
-
-
-  /**
-   * Updates the ico from the model.
-   */
-  setIconFromModel: function() {
-    if (!goog.isNull(this.getModel())) {
-      this.setIconInternal(
-          /** @type {pstj.material.Icon.Name} */ (
-          this.getModel().getProp('icon') ||
-          pstj.material.Icon.Name.NONE));
-    }
-  },
-
-
-  /**
-   * Sets the icon to be used with the menu item. If set to NONE the icon will
-   * be hidden.
-   * @param {pstj.material.Icon.Name} icon
-   */
-  setIconInternal: function(icon) {
-    this.setEmpty(icon == pstj.material.Icon.Name.NONE);
-    if (this.isInDocument()) {
-      this.getChildAt(0).setIcon(icon);
-    }
+  setIcon: function(icon) {
+    this.setEmpty(icon == pstj.material.icon.Name.NONE);
+    goog.asserts.assertInstanceof(this.getChildAt(0),
+        pstj.material.IconContainer).setIcon(icon);
   },
 
 
@@ -102,29 +49,14 @@ pstj.material.MenuItem = goog.defineClass(pstj.material.Element, {
       this.getChildAt(1).setContent(c);
     }
     this.setContentInternal(c);
-  },
-
-
-  statics: {
-    fromJSON: function(config) {
-      var i = new pstj.material.MenuItem();
-      i.setModel(new pstj.ds.ListItem(config));
-      return i;
-    }
   }
 });
 
 
-
-/**
- * Implements the renderer for the element.
- * @constructor
- * @struct
- * @extends {pstj.material.ElementRenderer}
- */
+/** @extends {pstj.material.ElementRenderer} */
 pstj.material.MenuItemRenderer = goog.defineClass(ER, {
   constructor: function() {
-    goog.base(this);
+    ER.call(this);
   },
 
 
@@ -139,9 +71,19 @@ pstj.material.MenuItemRenderer = goog.defineClass(ER, {
     return pstj.material.template.MenuItem(model);
   },
 
+  /** @override */
+  generateTemplateData: function(inst) {
+    var content = inst.getContent();
+    if (!goog.isString(content)) {
+      content = '';
+    }
+    return {
+      content: content,
+      icon: 'none'
+    };
+  },
+
   statics: {
-
-
     /**
      * @type {string}
      * @final
