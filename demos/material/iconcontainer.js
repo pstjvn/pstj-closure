f8be70010498c08b8894700ef5658a157703fe2a
Peter StJ <regardingscot@gmail.com>
2015-05-14 18:22:43 +0300

    Add more icons
    Also add the icon mutation in demos

--- demos/material/iconcontainer.js
+++ demos/material/iconcontainer.js
@@ -6,7 +6,14 @@ goog.require('pstj.material.IconContainer');
 (function() {
   goog.ui.decorate(document.querySelector('[is]'));
 
+  var ci = 'arrow-drop-down';
+
   var ic = new pstj.material.IconContainer();
-  ic.setIcon('menu');
+  ic.setIcon(ci);
   ic.render(document.querySelector('.rendertarget'));
+
+  ic.getElement().addEventListener('click', function() {
+    ci = (ci == 'arrow-drop-down' ? 'arrow-drop-up' : 'arrow-drop-down');
+    ic.setIcon(ci);
+  });
 })();
