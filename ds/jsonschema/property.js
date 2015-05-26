32d8245dd01cd104c921fcefc75b1ea69f7d18fe
Peter StJ <regardingscot@gmail.com>
2015-05-26 17:54:53 +0300

    Only call toJSON for referenced types, skip for primitives

--- ds/jsonschema/property.js
+++ ds/jsonschema/property.js
@@ -452,12 +452,13 @@ pstj.ds.jsonschema.Property = goog.defineClass(null, {
       if (this.type == 'string') return sjsname + '.toString()';
       if (this.type == 'number') return 'parseFloat(' + sjsname + ')';
       if (this.type == 'object' || this.type == 'array') {
-        if (this.type == 'array') {
+        if (this.type == 'array' && this.isReferencedType()) {
           return 'goog.array.map(' + sjsname + ', function(item) {' +
               '\n        return item.toJSON();' +
               '\n      })';
+        } else {
+          return sjsname;
         }
-        return sjsname;
       }
     }
     return 'ERROR';
